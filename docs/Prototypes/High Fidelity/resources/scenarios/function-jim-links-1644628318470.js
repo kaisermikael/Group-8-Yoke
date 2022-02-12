921bb2400f54d9636bb52beb6f769481f2457a68
VoodooWonderBot <awesome.robotman@gmail.com>
2022-02-12 02:29:41 +0000

    created prototype and hopefully branched properly

---
+++ docs/Prototypes/High Fidelity/resources/scenarios/function-jim-links-1644628318470.js
@@ -0,0 +1,17 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "5237f0ed-2a2a-4e16-882f-d49d50db055c" : {
+      "Button_1" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ]
+    },
+    "d12245cc-1680-458d-89dd-4f0d7fb22724" : {
+      "Button" : [
+        "5237f0ed-2a2a-4e16-882f-d49d50db055c"
+      ]
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
